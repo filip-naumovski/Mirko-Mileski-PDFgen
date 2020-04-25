46d2f5d87ef0488db26ba2e870db8f2623a41c93
Filip Naumovski <filip.naumovski00@live.com>
2020-04-25 19:13:41 +0200

    Test

---
+++ mirmil/Pages/Home.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using FirstFloor.ModernUI.Presentation;
+using System.Windows.Controls.Primitives;
+using FirstFloor.ModernUI.Windows.Controls;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Font;
+
+namespace mirmil.Pages
+{
+    public partial class Home : UserControl
+    {
+        
+        public Home()
+        {
+            InitializeComponent();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                @"Data Source=localhost\sqlexpress;" +
+                "Initial Catalog=Mirko Mileski;" +
+                "User id=mirko;" +
+                "Password=mirko;";
+            AppearanceManager.Current.ThemeSource = AppearanceManager.DarkThemeSource;
+            conn.Open();
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = conn;
+            comm.CommandText = ("SELECT * FROM Uchenici");
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            DataTable datatable = new DataTable();
+            adapter.Fill(datatable);
+            datatable.Columns.Add("Datum na raganje", typeof(System.String));
+            foreach (DataRow row in datatable.Rows)
+            {
+                DateTime date = DateTime.Parse(row["Datum na ragjanje"].ToString());
+                row["Datum na raganje"] = date.ToString("dd/MM/yyyy");
+            }
+            datatable.Columns.Remove("Datum na ragjanje");
+            var myStyle = new System.Windows.Style(typeof(DataGridCell))
+            {
+                Setters = {
+                     new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center)
+                }
+            };
+            Uchenici.CellStyle = myStyle;
+            Uchenici.DataContext = datatable.DefaultView;
+        }
+
+        
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OnAddClick window = new OnAddClick();
+            window.InitializeComponent();
+            window.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                @"Data Source=localhost\sqlexpress;" +
+                "Initial Catalog=Mirko Mileski;" +
+                "User id=mirko;" +
+                "Password=mirko;";
+            AppearanceManager.Current.ThemeSource = AppearanceManager.DarkThemeSource;
+            conn.Open();
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = conn;
+            comm.CommandText = ("SELECT * FROM Uchenici");
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            DataTable datatable = new DataTable();
+            adapter.Fill(datatable);
+            datatable.Columns.Add("Datum na raganje", typeof(System.String));
+            foreach (DataRow row in datatable.Rows)
+            {
+                DateTime date = DateTime.Parse(row["Datum na ragjanje"].ToString());
+                row["Datum na raganje"] = date.ToString("dd/MM/yyyy");
+            }
+            datatable.Columns.Remove("Datum na ragjanje");
+            var myStyle = new System.Windows.Style(typeof(DataGridCell))
+            {
+                Setters = {
+                     new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center)
+                }
+            };
+            DataView myView = new DataView();
+            myView = datatable.DefaultView;
+            Uchenici.CellStyle = myStyle;
+            Uchenici.ItemsSource = myView;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView selectedRowView = (DataRowView) Uchenici.SelectedItem;
+            DataRow selectedRow = selectedRowView.Row;
+            string docIme = selectedRow["Ime"].ToString() + selectedRow["Prezime"].ToString();
+            string dest = "../../pdf/" + docIme + ".pdf";
+            dest.Trim();
+            string destFinal = string.Concat(dest.Where(c => !char.IsWhiteSpace(c)));
+            PdfDocument pdf = new PdfDocument(new PdfReader("../../pdf/testform.pdf"), new PdfWriter(destFinal));
+            PdfAcroForm form = PdfAcroForm.GetAcroForm(pdf, true);
+            IDictionary<string, PdfFormField> fields = new Dictionary<string, PdfFormField>();
+            fields = form.GetFormFields();
+            string FONT = "../../fonts/OpenSans-Regular.ttf";
+            PdfFont font1 = PdfFontFactory.CreateFont(FONT, "Cp1251", true);
+            fields["tb1"].SetValue(selectedRow["Ime"].ToString(), font1, 10);
+            fields["tb2"].SetValue(selectedRow["Prezime"].ToString(), font1, 10);
+            fields["tb3"].SetValue(selectedRow["Tatkovo ime"].ToString(), font1, 10);
+            fields["tb4"].SetValue(selectedRow["Datum na ragjanje"].ToString(), font1, 10);
+            form.FlattenFields();
+            pdf.Close();
+            MessageBox.Show("Успешно е експортиран фајлот.");
+        }
+        string searchText;
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchText = searchBox.Text;
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                @"Data Source=localhost\sqlexpress;" +
+                "Initial Catalog=Mirko Mileski;" +
+                "User id=mirko;" +
+                "Password=mirko;";
+            AppearanceManager.Current.ThemeSource = AppearanceManager.DarkThemeSource;
+            conn.Open();
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = conn;
+            comm.CommandText = ("SELECT * FROM Uchenici");
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            DataTable datatable = new DataTable();
+            adapter.Fill(datatable);
+            datatable.Columns.Add("Datum na raganje", typeof(System.String));
+            foreach (DataRow row in datatable.Rows)
+            {
+                DateTime date = DateTime.Parse(row["Datum na ragjanje"].ToString());
+                row["Datum na raganje"] = date.ToString("dd/MM/yyyy");
+            }
+            datatable.Columns.Remove("Datum na ragjanje");
+            var myStyle = new System.Windows.Style(typeof(DataGridCell))
+            {
+                Setters = {
+                     new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center)
+                }
+            };
+            string[] searchParam = searchText.Split(' ');
+            int wordCount = 0;
+            foreach(string item in searchParam)
+            {
+                wordCount++;
+            }
+            DataView myView = new DataView();
+            myView = datatable.DefaultView;
+            if (wordCount != 1 && wordCount != 2)
+            {
+                MessageBox.Show("Greshna search sintaksa.");
+                Uchenici.CellStyle = myStyle;
+                Uchenici.ItemsSource = myView;
+                return;
+            }
+            else if(wordCount == 1)
+            {
+                myView.RowFilter = "Ime LIKE '%" + searchParam[0] + "%'";
+                Uchenici.CellStyle = myStyle;
+                Uchenici.ItemsSource = myView;
+                return;
+
+            }
+            else if(wordCount == 2)
+            {
+                myView.RowFilter = "Ime LIKE '%" + searchParam[0] + "%'" + "AND Prezime LIKE '%" + searchParam[1] + "%'";
+                Uchenici.CellStyle = myStyle;
+                Uchenici.ItemsSource = myView;
+                return;
+            }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+        private void SearchBox_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SearchButton_Click(this, new RoutedEventArgs());
+            }
+        }
+    }
+}
---
+++ mirmil/Pages/OnAddClick.xaml.cs
@@ -0,0 +1,159 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mirmil.Pages
+{
+    public partial class OnAddClick : ModernWindow
+    {
+        public OnAddClick()
+        {
+            InitializeComponent();
+            Style = (Style)App.Current.Resources["BlankWindow"];
+         }
+        bool imeWrite = false;
+        bool prezimeWrite = false;
+        bool tatkovoWrite = false;
+        bool datumWrite = false;
+
+        //Shminkanje
+
+        private void Ime_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!imeWrite) Ime.Text = "";
+            imeWrite = true;
+        }
+        private void Prezime_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if(!prezimeWrite) Prezime.Text = "";
+            prezimeWrite = true;
+        }
+        private void Tatkovo_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if(!tatkovoWrite) Tatkovoime.Text = "";
+            tatkovoWrite = true;
+        }
+        private void Datum_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if(!datumWrite) Datum.Text = "";
+            datumWrite = true;
+        }
+        private void Ime_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Prezime.SelectAll();
+            }
+            
+        }
+        private void Prezime_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Tatkovoime.SelectAll();
+            }
+        }
+        private void Tatkovo_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Datum.SelectAll();
+            }
+        }
+        private void Datum_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                button_Click(this, new RoutedEventArgs());
+            }
+
+        }
+
+        private void Ime_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            //if (Ime.Text == "") Ime.Visibility = Visibility.Visible;
+        }
+        private void Prezime_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            //if (Prezime.Text == "") Prezime.Visibility = Visibility.Visible;
+        }
+        private void Tatkovo_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            //if (Tatkovoime.Text == "") Tatkovoime.Visibility = Visibility.Visible;
+        }
+        private void Datum_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            //if (Datum.Text == "") Datum.Visibility = Visibility.Visible;
+        }
+
+        //Funkcionalnost
+
+        string newIme="";
+        string newPrezime="";
+        string newTatkovo="";
+        string newDatum="";
+
+        private void Ime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Prezime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Tatkovoime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Datum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (newIme == "" || newPrezime == "" || newTatkovo == "" || newDatum == "")
+            {
+                this.Hide();
+                return;
+            }
+            newIme = Ime.Text;
+            newPrezime = Prezime.Text;
+            newTatkovo = Tatkovoime.Text;
+            newDatum = Datum.Text;
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                @"Data Source=localhost\sqlexpress;" +
+                "Initial Catalog=Mirko Mileski;" +
+                "User id=mirko;" +
+                "Password=mirko;";
+            string sqlQuery = "INSERT INTO Uchenici VALUES(@newIme,@newPrezime,@newTatkovo,@newDatum);";
+            SqlCommand comm = new SqlCommand(sqlQuery, conn);
+            conn.Open();
+            comm.Parameters.AddWithValue("newIme", newIme);
+            comm.Parameters.AddWithValue("newPrezime", newPrezime);
+            comm.Parameters.AddWithValue("newTatkovo", newTatkovo);
+            comm.Parameters.AddWithValue("newDatum", newDatum);
+            comm.ExecuteNonQuery();
+            this.Hide();
+        }
+    }
+}
+
